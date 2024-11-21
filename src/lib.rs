fe4b667dc87329978508031706ef8f948265bf29
Valetoriy <shadolproff420@mail.ru>
2024-11-21 15:11:55 +0300

    Добавить debug-плагины

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,36 @@
-use bevy::prelude::*;
+#[cfg(debug_assertions)]
+mod dev_tools;
+
+use bevy::{audio::Volume, prelude::*};
+
+#[derive(States, Debug, PartialEq, Eq, Clone, Hash, Default)]
+enum GameState {
+    #[default]
+    // Splash,
+    // Loading,
+    // Menu,
+    Gameplay,
+}
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
-    fn build(&self, _: &mut App) {
+    fn build(&self, app: &mut App) {
+        app.init_state::<GameState>();
+        app.enable_state_scoped_entities::<GameState>();
+
+        app.add_systems(Startup, spawn_camera);
+
+        app.insert_resource(GlobalVolume {
+            volume: Volume::new(0.3),
+            ..default()
+        });
+
+        #[cfg(debug_assertions)]
+        app.add_plugins(dev_tools::plugin);
     }
+}
+
+fn spawn_camera(mut cmd: Commands) {
+    cmd.spawn((Camera2dBundle::default(), IsDefaultUiCamera));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
-use bevy::prelude::*;
+// Выключить коммандную строку в релиз-сборках для Windows
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use bevy::{
+    asset::AssetMetaCheck, prelude::*, window::PrimaryWindow, winit::WinitWindows,
+};
+use boyar_tournament::GamePlugin;
+use std::io::Cursor;
+use winit::window::Icon;
 
 fn main() {
-    App::new().add_systems(Startup, hello_world).run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Window {
+                        title: "Боярский Турнир".into(),
+                        ..default()
+                    }
+                    .into(),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                }),
+        )
+        .add_plugins(GamePlugin)
+        .add_systems(Startup, set_window_icon)
+        .run();
 }
 
-fn hello_world() {
-    println!("rusi protiv yasherov");
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) {
+    let primary_window = primary_window.single();
+    let Some(primary_window) = windows.get_window(primary_window) else {
+        return;
+    };
+    let icon_buf = Cursor::new(include_bytes!("../resources/desktop_icon.png"));
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary_window.set_window_icon(Some(icon));
+    }
 }
