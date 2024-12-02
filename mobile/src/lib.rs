f1275f04cdb768a8912c827a1a47f1de5d2c17fa
Krasin1 <dunaevv84@gmail.com>
2024-12-02 14:29:51 +0300

    Перейти на bevy 0.15 в мобильной версии

--- mobile/src/lib.rs
+++ mobile/src/lib.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
-use bevy::window::WindowMode;
+use bevy::window::{AppLifecycle, WindowMode};
+use bevy::winit::WinitSettings;
 use boyar_tournament::GamePlugin;
 
 #[bevy_main]
@@ -10,7 +11,7 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Window {
                         resizable: false,
-                        mode: WindowMode::BorderlessFullscreen,
+                        mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
                         ..default()
                     }
                     .into(),
@@ -19,5 +20,22 @@ fn main() {
                 .set(ImagePlugin::default_nearest()),
             GamePlugin,
         ))
+        .insert_resource(WinitSettings::mobile())
+        .add_systems(Update, handle_lifetime)
         .run();
+}
+
+/// Остановка звука при переходе приложения в фоновый режим
+// Взято из официального android примера, не проверял нужно или нет
+fn handle_lifetime(
+    mut lifecycle_events: EventReader<AppLifecycle>,
+    music_controller: Single<&AudioSink>,
+) {
+    for event in lifecycle_events.read() {
+        match event {
+            AppLifecycle::Idle | AppLifecycle::WillSuspend | AppLifecycle::WillResume => {}
+            AppLifecycle::Suspended => music_controller.pause(),
+            AppLifecycle::Running => music_controller.play(),
+        }
+    }
 }
